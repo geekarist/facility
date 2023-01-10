1091bfd409c34010c2e5867b9ca3bc68335f2ec0
christoufair <geekarist@users.noreply.github.com>
2023-01-10 09:05:59 +0100

    Add Oolong dependency

--- singleplatform/build.gradle.kts
+++ singleplatform/build.gradle.kts
@@ -27,6 +27,7 @@ kotlin {
         val jvmMain by getting {
             dependencies {
                 implementation(compose.desktop.currentOs)
+                implementation("org.oolong-kt:oolong:2.1.1")
             }
         }
         val jvmTest by getting
