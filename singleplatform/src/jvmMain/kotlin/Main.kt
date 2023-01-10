c7adfc2194a955b7a7e6370bfa484ed6720b489e
christoufair <geekarist@users.noreply.github.com>
2023-01-10 18:55:02 +0100

    Use Oolong.runtime

--- singleplatform/src/jvmMain/kotlin/Main.kt
+++ singleplatform/src/jvmMain/kotlin/Main.kt
@@ -1,15 +1,16 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.CoroutineScope
+import oolong.Dispatch
+import oolong.effect.none
+import oolong.next.next
+import oolong.runtime
 
 @Composable
 @Preview
@@ -27,17 +28,39 @@ fun App(props: App.Props?) {
 
 fun main() {
     application {
-        val appProps by rememberSaveable { mutableStateOf<App.Props?>(null) }
+        var appProps by rememberSaveable { mutableStateOf<App.Props?>(null) }
         Window(onCloseRequest = ::exitApplication) {
             App(appProps)
         }
 
         LaunchedEffect(Unit) {
-            // TODO: launch Oolong runtime
+            runtime(
+                init = App::init,
+                update = App::update,
+                view = App::view,
+                render = { props: App.Props ->
+                    appProps = props
+                    null
+                })
         }
     }
 }
 
 object App {
-    data class Props(val text: String)
+    fun init() = next<_, Event>(Model("Yo"))
+
+    fun update(event: Event, model: Model): Pair<Model, suspend CoroutineScope.(Dispatch<Event>) -> Any?> {
+        println(event)
+        return model to none()
+    }
+
+    class Event {
+
+    }
+
+    fun view(model: Model, dispatch: Dispatch<Event>): Props = Props(model.text, dispatch)
+
+    data class Model(val text: String)
+
+    data class Props(val text: String, val dispatch: Dispatch<Event>)
 }
