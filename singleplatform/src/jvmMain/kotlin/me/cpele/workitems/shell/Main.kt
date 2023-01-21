32f7ecf267f2de0398ac93187e9c3d943a52ac59
christoufair <geekarist@users.noreply.github.com>
2023-01-21 07:30:05 +0100

    Textual UI

--- singleplatform/src/jvmMain/kotlin/me/cpele/workitems/shell/Main.kt
+++ singleplatform/src/jvmMain/kotlin/me/cpele/workitems/shell/Main.kt
@@ -1,10 +1,14 @@
 package me.cpele.workitems.shell
 
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import me.cpele.workitems.core.App
@@ -13,13 +17,11 @@ import oolong.runtime
 @Composable
 fun Ui(props: App.Props?) {
     MaterialTheme {
-        props?.let {
-            Button(onClick = {
-                props.dispatch(App.Event.ButtonClicked)
-            }) {
+        Box(Modifier.fillMaxSize().padding(32.dp)) {
+            props?.let {
                 Text(props.text)
-            }
-        } ?: Text("Loading...")
+            } ?: Text("Loading...")
+        }
     }
 }
 
