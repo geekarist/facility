3182c8252a92512d348642ff14d1aff068bd1f3c
christoufair <geekarist@users.noreply.github.com>
2023-01-23 08:48:08 +0100

    Provide render context

--- singleplatform/src/jvmMain/kotlin/me/cpele/workitems/shell/WorkItemsApp.kt
+++ singleplatform/src/jvmMain/kotlin/me/cpele/workitems/shell/WorkItemsApp.kt
@@ -1,7 +1,13 @@
 package me.cpele.workitems.shell
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import me.cpele.workitems.core.WorkItems
@@ -10,13 +16,18 @@ import oolong.runtime
 object WorkItemsApp {
     @Composable
     fun Ui(props: WorkItems.Props?) {
-        TODO()
+        MaterialTheme {
+            Box(Modifier.padding(32.dp)) {
+                Text("Yo")
+            }
+        }
     }
 
     fun run() = application {
         var workItemsProps by rememberSaveable {
             mutableStateOf<WorkItems.Props?>(null)
         }
+        val coroutineScope = rememberCoroutineScope()
         Window(onCloseRequest = ::exitApplication) {
             Ui(workItemsProps)
         }
@@ -27,7 +38,8 @@ object WorkItemsApp {
                 view = WorkItems::view,
                 render = {
                     it.also { workItemsProps = it }
-                }
+                },
+                renderContext = coroutineScope.coroutineContext
             )
         }
     }
