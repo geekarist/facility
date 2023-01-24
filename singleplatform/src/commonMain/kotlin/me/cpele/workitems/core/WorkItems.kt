d07666e9b995ce71da8187d693741d94a43037a1
christoufair <geekarist@users.noreply.github.com>
2023-01-24 08:22:53 +0100

    Implement loading, success status

--- singleplatform/src/commonMain/kotlin/me/cpele/workitems/core/WorkItems.kt
+++ singleplatform/src/commonMain/kotlin/me/cpele/workitems/core/WorkItems.kt
@@ -1,38 +1,70 @@
 package me.cpele.workitems.core
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 import oolong.Dispatch
+import oolong.effect
 import oolong.effect.none
 
 object WorkItems {
-    fun init() = Model(
-        items = listOf(
-            Model.Item(
-                title = "First item",
-                desc = "First item description",
-                status = Model.Item.Status.ToDo,
-            )
-        )
-    ) to none<Event>()
+    fun init() = Model(items = listOf(), status = Model.Status.Loading) to
+            effect { dispatch ->
+                dispatch(Event.LoadingStarted)
+                delay(2000)
+                val newItems =
+                    listOf(
+                        Model.Item(
+                            title = "First loaded item",
+                            desc = "First loaded item description",
+                            status = Model.Item.Status.ToDo
+                        ),
+                        Model.Item(
+                            title = "Second loaded item",
+                            desc = "Second loaded item description",
+                            status = Model.Item.Status.ToDo
+                        ),
+                        Model.Item(
+                            title = "Third loaded item",
+                            desc = "Third loaded item description",
+                            status = Model.Item.Status.InProgress
+                        ),
+                        Model.Item(
+                            title = "Fourth loaded item",
+                            desc = "Fourth loaded item description",
+                            status = Model.Item.Status.Done
+                        )
+                    )
+                dispatch(Event.ItemsLoaded(newItems))
+            }
 
     fun update(
         event: Event,
         model: Model
-    ): Pair<Model, suspend CoroutineScope.(Dispatch<Event>) -> Any?> {
-        TODO("Not yet implemented")
+    ): Pair<Model, suspend CoroutineScope.(Dispatch<Event>) -> Any?> = when (event) {
+        Event.LoadingStarted -> model.copy(status = Model.Status.Loading) to none()
+        is Event.ItemsLoaded -> model.copy(status = Model.Status.Success, items = event.items) to none()
     }
 
     fun view(model: Model, function: (Event) -> Unit) = Props()
 
-    class Model(val items: List<Item>) {
-        class Item(val title: String, val desc: String, val status: Status) {
+    data class Model(val status: Status, val items: List<Item>) {
+        data class Item(val title: String, val desc: String, val status: Status) {
             enum class Status {
                 ToDo, InProgress, Done
             }
         }
+
+        sealed class Status {
+            object Loading : Status()
+            data class Failure(val t: Throwable) : Status()
+            object Success : Status()
+        }
     }
 
-    class Event
-    class Props
+    sealed class Event {
+        object LoadingStarted : Event()
+        class ItemsLoaded(val items: List<Model.Item>) : Event()
+    }
 
+    class Props
 }
