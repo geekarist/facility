db149ce26741a5452d539d73a0b8d7d6813d4631
christoufair <geekarist@users.noreply.github.com>
2023-01-24 07:39:38 +0100

    Implement model

--- singleplatform/src/commonMain/kotlin/me/cpele/workitems/core/WorkItems.kt
+++ singleplatform/src/commonMain/kotlin/me/cpele/workitems/core/WorkItems.kt
@@ -5,7 +5,15 @@ import oolong.Dispatch
 import oolong.effect.none
 
 object WorkItems {
-    fun init() = Model() to none<Event>()
+    fun init() = Model(
+        items = listOf(
+            Model.Item(
+                title = "First item",
+                desc = "First item description",
+                status = Model.Item.Status.ToDo,
+            )
+        )
+    ) to none<Event>()
 
     fun update(
         event: Event,
@@ -16,7 +24,14 @@ object WorkItems {
 
     fun view(model: Model, function: (Event) -> Unit) = Props()
 
-    class Model
+    class Model(val items: List<Item>) {
+        class Item(val title: String, val desc: String, val status: Status) {
+            enum class Status {
+                ToDo, InProgress, Done
+            }
+        }
+    }
+
     class Event
     class Props
 
