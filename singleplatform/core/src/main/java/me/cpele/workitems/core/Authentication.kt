3036b34298a60860d07c3db35ca1ad33b7b39591
christoufair <geekarist@users.noreply.github.com>
2023-02-07 08:35:34 +0100

    Empty implementations

--- singleplatform/core/src/main/java/me/cpele/workitems/core/Authentication.kt
+++ singleplatform/core/src/main/java/me/cpele/workitems/core/Authentication.kt
@@ -2,30 +2,21 @@ package me.cpele.workitems.core
 
 import kotlinx.coroutines.CoroutineScope
 import oolong.Effect
+import oolong.effect.none
 
 object Authentication {
-    fun init(): Pair<Model, Effect<Event>> {
-        TODO("Not yet implemented")
-    }
+    fun init(): Pair<Model, Effect<Event>> = Model() to none()
 
     fun update(
         event: Event,
         model: Model
-    ): Pair<Model, suspend CoroutineScope.((Event) -> Unit) -> Any?> {
-        TODO("Not yet implemented")
-    }
+    ): Pair<Model, suspend CoroutineScope.((Event) -> Unit) -> Any?> = model to none()
 
-    fun view(model: Model, function: (Event) -> Unit): Props {
-        TODO("Not yet implemented")
-    }
+    fun view(model: Model, function: (Event) -> Unit): Props = Props()
 
-    class Event {
+    class Event
 
-    }
-
-    class Model {
-
-    }
+    class Model
 
     class Props(val text: String = "Yo")
 }
