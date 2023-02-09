bb4294f71c1be64c0dff4541dc9d7aa0102aee7c
christoufair <geekarist@users.noreply.github.com>
2023-02-09 07:57:02 +0100

    More comments

--- singleplatform/core/src/main/java/me/cpele/workitems/core/Authentication.kt
+++ singleplatform/core/src/main/java/me/cpele/workitems/core/Authentication.kt
@@ -10,25 +10,27 @@ import oolong.effect.none
  * ## Model
  *
  * - List of providers
- * - Provider has description, optional account
+ * - Provider has description, optional account, interface
  * - Account has info like login, tokens, avatar, etc.
+ * - Auth status: absent, pending, failure, complete
+ * - Status message
  *
  * ## Update
  *
  * - Initially no provider
- * - Login requested for given provider ⇒ send request to provider, record pending auth status
- * - Provider authenticated account ⇒ store account info, record completed auth status
- * - Error requesting account ⇒ TODO
- * - Auth request refused ⇒ TODO
- * - Provider info requested ⇒ TODO (account info or provider info)
- * - Logout requested for provider ⇒ TODO
+ * - Login requested for given provider ⇒ contact provider to initiate login, record pending auth status
+ * - Provider authenticated account ⇒ record account info, record completed auth status
+ * - Error requesting account ⇒ record error status message
+ * - Auth request refused ⇒ record failure status message
+ * - Provider info requested ⇒ expose appropriate info, either about provider or account
+ * - Logout requested ⇒ contact provider to request logout, record deleted auth status
  *
  * ## View
  *
- * - List of icons, each of them a provider of work items
- * like Slack, Jira, etc.
- * - Provider without account displays as grayscale icon. On click shows *provider* info, *login* button.
- * - Provider with account displays as color icon. On click shows *account* info, *logout* button.
+ * - List of icons, each of them a provider of work items like Slack, Jira, etc.
+ * - Provider without account displays as grayscale icon. On click show *provider* info, *login* button.
+ * - Provider with account displays as color icon. On click show *account* info, *logout* button.
+ * - Optional error message modal dialog
  */
 object Authentication {
     fun init(): Pair<Model, Effect<Event>> = Model(emptyList()) to none()
