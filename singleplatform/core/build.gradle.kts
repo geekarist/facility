53df9ee2f70cbd29f9231f01ee82421ea1096b20
christoufair <geekarist@users.noreply.github.com>
2023-03-21 16:36:24 +0100

    Add serialization lib dependency

--- singleplatform/core/build.gradle.kts
+++ singleplatform/core/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
 }
-
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
@@ -10,6 +9,7 @@ java {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.oolong-kt:oolong:2.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 }
 repositories {
     mavenCentral()
