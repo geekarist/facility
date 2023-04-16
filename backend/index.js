2be53b8ad3feaf7e2c6a98d154d21c4c19344eae
christoufair <geekarist@users.noreply.github.com>
2023-04-16 07:27:36 +0200

    Implement HTTPS

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const https = require('https');
 const app = express();
 
 app.get('/code-ack', (req, res) => {
@@ -12,15 +13,17 @@ app.get('/code-ack', (req, res) => {
     // Make an HTTP GET request to the actual endpoint using the 'axios' library
     const axios = require('axios');
     axios.get(actualEndpointUrl)
-    .then(response => {
-        res.send(response.data);
+        .then(response => {
+            res.send(response.data);
         })
-    .catch(error => {
-        res.status(500).send(`Error getting ${actualEndpointUrl}: ${error}`);
+        .catch(error => {
+            res.status(500).send(`Error getting ${actualEndpointUrl}: ${error}`);
         });
-    });
+});
+
+const httpsServer = https.createServer({}, app);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+httpsServer.listen(port, () => {
     console.log(`Wrapper endpoint listening on port ${port}`);
-    });
+});
