b4caeda6137f70085274808e2e205be6df1cdf4a
christoufair <geekarist@users.noreply.github.com>
2023-05-15 09:04:05 +0200

    Create SlackAccount

---
+++ singleplatform/core/src/main/java/me/cpele/workitems/core/SlackAccount.kt
@@ -0,0 +1,69 @@
+package me.cpele.workitems.core
+
+import me.cpele.workitems.core.SlackAccount.Model.Authorized
+import me.cpele.workitems.core.SlackAccount.Model.Invalid
+import oolong.effect.none
+
+object SlackAccount {
+
+    fun init() = Model.Blank to none<Event>()
+
+    fun makeUpdate(
+        slack: Slack,
+        platform: Platform
+    ): (model: Model, event: Event) -> Change<Model, Event> = { model, event ->
+        when (event) {
+            else -> model to none()
+        }
+    }
+
+    fun view(model: Model) = when (model) {
+        is Model.Blank -> Props(
+            // "Sign in..." button, enabled
+        )
+
+        Model.Invalid -> Props(
+            // "Retry sign-in..." button, enabled
+            // Dialog with partial account data, authentication status detail
+        )
+
+        Model.Pending -> Props(
+            // "Signing in" button, disabled
+            // Dialog with partial account data, authentication status detail
+        )
+
+        Model.Authorized -> Props(
+            // "Sign out" button, enabled
+            // Dialog with account data
+        )
+    }
+
+    /**
+     * This model represents a Slack user account.
+     *
+     * I can be in the following states:
+     * user info or authentication data
+     * - [Invalid]: Authentication failed at some point
+     * - [Authorized]: Authentication succeeded so the account has authentication data and user info
+     */
+    sealed interface Model {
+        /** Authentication process wasn't even started */
+        object Blank : Model
+
+        /** Authentication started but not complete */
+        object Pending : Model
+
+        /** Authentication failed at some point */
+        object Invalid : Model
+
+        /** Authentication was successful */
+        object Authorized : Model
+    }
+
+    class Event {
+    }
+
+    class Props {
+
+    }
+}
