7fb06794e868b267d6bed815ee7366a7a50618e2
christoufair <geekarist@users.noreply.github.com>
2023-05-16 18:58:59 +0200

    Move on to Pending

--- singleplatform/core/src/main/java/me/cpele/workitems/core/SlackAccount.kt
+++ singleplatform/core/src/main/java/me/cpele/workitems/core/SlackAccount.kt
@@ -9,25 +9,24 @@ object SlackAccount {
     fun init() = Change<Model, _>(Model.Blank, none<Event>())
 
     fun makeUpdate(
-        slack: Slack,
-        platform: Platform
+        slack: Slack, platform: Platform
     ): (Event, Model) -> Change<Model, Event> = { event, model ->
         when (event) {
-            Event.SignInRequested -> Change(model, effect {
-                platform.logi { "Got $event" }
-                slack.requestAuthScopes().collect { status ->
-                    platform.logi { "Got status $status" }
-                }
-            })
+            Event.SignInRequested -> Change(
+                Model.Pending,
+                effect {
+                    platform.logi { "Got $event" }
+                    slack.requestAuthScopes().collect { status ->
+                        platform.logi { "Got status $status" }
+                    }
+                })
         }
     }
 
     fun view(model: Model, dispatch: Dispatch<Event>) = when (model) {
-        is Model.Blank -> Props(
-            Button(text = "Sign into Slack", isEnabled = true) {
-                dispatch(Event.SignInRequested)
-            }
-        )
+        is Model.Blank -> Props(Button(text = "Sign into Slack", isEnabled = true) {
+            dispatch(Event.SignInRequested)
+        })
 
         Model.Invalid -> Props(
             // "Retry sign-in..." button, enabled
