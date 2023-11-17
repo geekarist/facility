113c1095a24cf0591d9696ad94d81729c25c6160
christoufair <geekarist@users.noreply.github.com>
2023-11-17 08:25:30 +0100

    Provide mock launcher, improve UI

--- frontend/src/jvmMain/kotlin/me/cpele/facility/shell/FacilityExt.kt
+++ frontend/src/jvmMain/kotlin/me/cpele/facility/shell/FacilityExt.kt
@@ -1,8 +1,16 @@
 package me.cpele.facility.shell
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.window.Window
 import me.cpele.facility.core.framework.effects.AppRuntime
 import me.cpele.facility.core.programs.Facility
@@ -10,13 +18,18 @@ import me.cpele.facility.core.programs.Facility.Ctx
 import me.cpele.facility.core.programs.SlackAccount
 import kotlin.system.exitProcess
 
-fun Facility.main() {
+fun Facility.main(vararg args: String) {
+    val slack = if (args.contains("mock")) {
+        MockSlack
+    } else {
+        DefaultSlack(DesktopPlatform, NgrokIngress(DesktopPlatform))
+    }
     app(
         init = ::init,
         update = makeUpdate(
             Ctx.of(
                 DesktopPlatform,
-                DefaultSlack(DesktopPlatform, NgrokIngress(DesktopPlatform)),
+                slack,
                 AppRuntime.of { exitProcess(0) },
                 DesktopPreferences,
                 DesktopStore
@@ -31,11 +44,29 @@ fun Facility.main() {
 @Composable
 private fun Facility.Ui(props: Facility.Props) = run {
     Window(onCloseRequest = props.onWindowClose) {
-        Button(
-            onClick = props.openSlackAccount.onClick,
-            enabled = props.openSlackAccount.isEnabled
-        ) {
-            Text(text = props.openSlackAccount.text)
+        Box(contentAlignment = Companion.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+                Text(style = MaterialTheme.typography.h3, text = "Facility")
+                Text(
+                    style = MaterialTheme.typography.body1,
+                    textAlign = TextAlign.Center,
+                    text = """
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                    deserunt mollit anim id est laborum.
+                    """.trimIndent()
+                )
+                Button(
+                    onClick = props.openSlackAccount.onClick,
+                    enabled = props.openSlackAccount.isEnabled
+                ) {
+                    Text(text = props.openSlackAccount.text)
+                }
+            }
         }
     }
     props.slackAccount?.let { subProps ->
@@ -46,4 +77,4 @@ private fun Facility.Ui(props: Facility.Props) = run {
 /**
  * Workaround to run the program easily from the IDE, because [Facility.main] fails when launched directly.
  */
-private fun main() = Facility.main()
+fun main(args: Array<String>) = Facility.main(*args)
